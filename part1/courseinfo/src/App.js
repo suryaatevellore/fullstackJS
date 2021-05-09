682c99fbbea0a6c55a47416ee536082baa2660cb
Surya Ahuja <sahuja4@fb.com>
2021-05-08 17:54:38 -0700

    Ex 1.4: Course array of objects

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -20,59 +20,49 @@ const Content = (props) => {
   console.log(props)
   return (
     <div>
-      <Part part={props.part[0].name} exercise={props.part[0].exercises} />
-      <Part part={props.part[1].name} exercise={props.part[1].exercises} />
-      <Part part={props.part[2].name} exercise={props.part[2].exercises} />
+      <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
     </div>
   )
 }
 
 const Total = (props) => {
+  let total = 0
+  props.total.forEach(
+    value => {
+      total += value.exercises
+    }
+  )
   return (
     <>
-      <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {total}</p>
     </>
   )
 }
 
-// const App = () => {
-//   const course = 'Half Stack application development'
-//   const part1 = 'Fundamentals of React'
-//   const exercises1 = 10
-//   const part2 = 'Using props to pass data'
-//   const exercises2 = 7
-//   const part3 = 'State of a component'
-//   const exercises3 = 14
-
-//   return (
-//     <div>
-//       <Header course={course} />
-//       <Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
-//       <Total total={exercises1 + exercises2 + exercises3} />
-//     </div>
-//   )
-// }
-
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part={[part1, part2, part3]} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total total={parts} />
     </div>
   )
 }
